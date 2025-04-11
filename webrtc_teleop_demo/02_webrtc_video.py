1eabe9c1a439badfbe6885f747dd91620e27c6a2
Ge Yang <ge.ike.yang@gmail.com>
2025-04-11 11:03:11 -0400

    update the docs

--- webrtc_teleop_demo/02_webrtc_video.py
+++ webrtc_teleop_demo/02_webrtc_video.py
@@ -1,9 +1,17 @@
+"""
+1. launch this server
+2. set up a ngrok link to this server
+3. launch the rtc_server.py
+4. set up a ngrok link to the rtc_server.py
+
+"""
 from asyncio import sleep
 
+from dotvar import auto_load  # noqa
 from vuer import Vuer, VuerSession
 from vuer.schemas import Scene, WebRTCVideoPlane
 
-app = Vuer(host="0.0.0.0", cors_origins="*", static_root=".")
+app = Vuer(host="0.0.0.0", cors_origins="https://{VUER_NGROK_PREFIX}-dev.ngrok.app", static_root=".")
 
 @app.spawn(start=True)
 async def main(sess: VuerSession):
